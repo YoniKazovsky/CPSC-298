20b6182a5e64357775fdae523836cc0f8cefc65b
Yoni Kazovsky <112571596+YoniKazovsky@users.noreply.github.com>
2023-09-10 22:39:28 -0700

    Add files via upload

---
+++ Proj_HelloCpp/hello_cin.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+string strMessage;
+cout << "Please type a single word message" << endl;
+cin >> strMessage;
+cout << "Message: " << strMessage << endl;
+return 0;
+}
